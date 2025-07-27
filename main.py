366a5c7729a73c6f8ce5f651bfe16edc85657cd7
dankeschoen13 <marcobernacer@gmail.com>
2025-07-27 23:25:44 +0800

    New branch initial commit. Implements UI changes for new 'decode' feature.

--- main.py
+++ main.py
@@ -4,83 +4,102 @@ from encoder import Encoder
 
 FONT1 = ("Courier", 20)
 FONT2 = ("Helvetica", 15)
-BG = "#8681BD"
+BG = "#1E201E"
 
 
 # FUNC
 encoder = Encoder()
 
 def run_encoding():
-    output_area.delete("1.0", "end")
-    message = input_area.get('1.0', 'end-1c')
+    encode_output.delete("1.0", "end")
+    message = encode_input.get('1.0', 'end-1c')
     morse_code = encoder.morsify(message)
     if morse_code:
-        output_label.configure(text="Success!")
-        output_area.insert('end', morse_code)
+        encode_feedback.configure(text="Success!")
+        encode_output.insert('end', morse_code)
     else:
-        output_label.configure(text="Please input a message!")
-        output_area.insert('end', "🙄")
+        encode_feedback.configure(text="Please input a message!")
+        encode_output.insert('end', "🙄")
+
+
+def create_ui(parent, background_color, command, action):
+    parent.grid_columnconfigure(0, weight=1)
+    parent.configure(fg_color=background_color)
+
+    input_label = CTkLabel(
+        parent,
+        text="Your message:",
+        padx=10,
+        pady=10,
+        font=FONT1,
+    )
+    input_label.grid(column=0, row=0, sticky='n')
+
+    input_area = CTkTextbox(
+        parent,
+        width=250,
+        height=100
+    )
+    input_area.grid(column=0, row=1, sticky='n')
+
+    submit = CTkButton(
+        parent,
+        text=action,
+        font=FONT2,
+        width=100,
+        height=40,
+        fg_color='#471396',
+        command=command
+    )
+    submit.grid(column=0, row=3, sticky='s', pady=(15, 15))
+
+    output_area = CTkTextbox(
+        parent,
+        width=250,
+        height=100
+    )
+    output_area.grid(column=0, row=4, sticky='n')
+
+    output_label = CTkLabel(
+        parent,
+        text="",
+        padx=10,
+        pady=10,
+        font=FONT1,
+    )
+    output_label.grid(column=0, row=5, sticky='n')
+
+    return output_area, output_label, input_area
 
 
 # USER INTERFACE  ----
 app = CTk()
-app.title("Morse Code Converter 1.0")
+app.title("Morse Code Converter 1.1")
 app.minsize(400, 400)
 app.config(pady=50, padx=50, background=BG)
 app.grid_columnconfigure(0, weight=1)
-app.grid_columnconfigure(1, weight=2)
-app.grid_columnconfigure(2, weight=1)
+app.grid_rowconfigure(0, weight=1)
+
+tabview = CTkTabview(master=app)
+tabview.grid(column=0, row=0)
+
+encode_tab = tabview.add("Encode")
+decode_tab = tabview.add("Decode")
+
+
+encode_output, encode_feedback, encode_input = create_ui(encode_tab, '#56021F', run_encoding, "MAKE MORSE")
+
+decode_output, decode_feedback, decode_input = create_ui(decode_tab, '#004030', run_encoding, "UN-MORSE")
 
 # input area label
-input_label = CTkLabel(
-    app,
-    text="Your message:",
-    padx=10,
-    pady=10,
-    font=FONT1,
-    bg_color=BG
-)
-input_label.grid(column=1, row=0, sticky='n')
-# input area
-input_area = CTkTextbox(
-    app,
-    width=250,
-    height=100
-)
-input_area.grid(column=1, row=1, sticky='n')
 
+# input area
 
 # submit button
-submit = CTkButton(
-    app,
-    text="SUBMIT",
-    font=FONT2,
-    width=100,
-    height=40,
-    bg_color=BG,
-    fg_color='#471396',
-    command=run_encoding
-)
-submit.grid(column=1, row=3, sticky='s', pady=(15, 15))
-
 
 # output/results textarea
-output_area = CTkTextbox(
-    app,
-    width=250,
-    height=100
-)
-output_area.grid(column=1, row=4, sticky='n')
+
 # output/feedback_message
-output_label = CTkLabel(
-    app,
-    text="",
-    padx=10,
-    pady=10,
-    font=FONT1,
-    bg_color=BG
-)
-output_label.grid(column=1, row=5, sticky='n')
 
 
 app.mainloop()
