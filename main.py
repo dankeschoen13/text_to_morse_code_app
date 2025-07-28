e0d50fe654355cd50f23b22c83a55de66cab2658
dankeschoen13 <marcobernacer@gmail.com>
2025-07-28 10:09:35 +0800

    reorganize UI constants and make improvements in code layout

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from customtkinter import *
 from encoder import Encoder
 
+# === UI CONFIG / STYLE CONSTANTS ===
+LABEL_FONT = ("Courier", 20)
+BUTTON_FONT = ("Helvetica", 15)
+APP_BG = "#1E201E"
+TAB1_COLOR = "#56021F"
+TAB2_COLOR = "#004030"
+BUTTON_COLOR = "#471396"
 
-FONT1 = ("Courier", 20)
-FONT2 = ("Helvetica", 15)
-BG = "#1E201E"
-
-
-# FUNC
+# === FUNCTIONS ===
 encoder = Encoder()
 
 def run_encoding():
@@ -21,17 +23,16 @@ def run_encoding():
         encode_feedback.configure(text="Please input a message!")
         encode_output.insert('end', "🙄")
 
-
-def create_ui(parent, background_color, command, action):
+def create_ui(parent, tab_color, command, action):
     parent.grid_columnconfigure(0, weight=1)
-    parent.configure(fg_color=background_color)
+    parent.configure(fg_color=tab_color)
 
     input_label = CTkLabel(
         parent,
         text="Your message:",
         padx=10,
         pady=10,
-        font=FONT1,
+        font=LABEL_FONT,
     )
     input_label.grid(column=0, row=0, sticky='n')
 
@@ -45,10 +46,10 @@ def create_ui(parent, background_color, command, action):
     submit = CTkButton(
         parent,
         text=action,
-        font=FONT2,
+        font=BUTTON_FONT,
         width=100,
         height=40,
-        fg_color='#471396',
+        fg_color=BUTTON_COLOR,
         command=command
     )
     submit.grid(column=0, row=3, sticky='s', pady=(15, 15))
@@ -65,18 +66,17 @@ def create_ui(parent, background_color, command, action):
         text="",
         padx=10,
         pady=10,
-        font=FONT1,
+        font=LABEL_FONT,
     )
     output_label.grid(column=0, row=5, sticky='n')
 
     return output_area, output_label, input_area
 
-
-# USER INTERFACE  ----
+# === USER INTERFACE ===
 app = CTk()
 app.title("Morse Code Converter 1.1")
 app.minsize(400, 400)
-app.config(pady=50, padx=50, background=BG)
+app.config(pady=50, padx=50, background=APP_BG)
 app.grid_columnconfigure(0, weight=1)
 app.grid_rowconfigure(0, weight=1)
 
@@ -86,20 +86,7 @@ tabview.grid(column=0, row=0)
 encode_tab = tabview.add("Encode")
 decode_tab = tabview.add("Decode")
 
-
-encode_output, encode_feedback, encode_input = create_ui(encode_tab, '#56021F', run_encoding, "MAKE MORSE")
-
-decode_output, decode_feedback, decode_input = create_ui(decode_tab, '#004030', run_encoding, "UN-MORSE")
-
-# input area label
-
-# input area
-
-# submit button
-
-# output/results textarea
-
-# output/feedback_message
-
+encode_output, encode_feedback, encode_input = create_ui(encode_tab, TAB1_COLOR, run_encoding, "MAKE MORSE")
+decode_output, decode_feedback, decode_input = create_ui(decode_tab, TAB2_COLOR, run_encoding, "UN-MORSE")
 
 app.mainloop()
